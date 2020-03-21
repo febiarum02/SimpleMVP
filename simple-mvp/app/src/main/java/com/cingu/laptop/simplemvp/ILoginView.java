5ff3024f1fc682dbfef28dafdebcb1e24bfafbf7
febiarum02 <58418437+febiarum02@users.noreply.github.com>
2020-03-21 11:47:33 +0700

    Update ILoginView.java

--- simple-mvp/app/src/main/java/com/cingu/laptop/simplemvp/ILoginView.java
+++ simple-mvp/app/src/main/java/com/cingu/laptop/simplemvp/ILoginView.java
@@ -3,6 +3,4 @@ package com.cingu.laptop.simplemvp;
 public interface ILoginView {
     public void onClearText();
     public void onLoginResult(Boolean result, int code);
-
-    void onSetProgressBarVisibility(int visibility);
 }
