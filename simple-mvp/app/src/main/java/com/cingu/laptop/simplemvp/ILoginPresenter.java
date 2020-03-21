bebf9d937a3446038c408d0b235fd443f79ff450
febiarum02 <58418437+febiarum02@users.noreply.github.com>
2020-03-21 11:47:57 +0700

    Update ILoginPresenter.java

--- simple-mvp/app/src/main/java/com/cingu/laptop/simplemvp/ILoginPresenter.java
+++ simple-mvp/app/src/main/java/com/cingu/laptop/simplemvp/ILoginPresenter.java
@@ -3,7 +3,4 @@ package com.cingu.laptop.simplemvp;
 class ILoginPresenter {
     public void clear() {}
     public void doLogin(String toString, String toString1) {}
-
-    public void setProgressBarVisiblity(int invisible) {
-    }
 }
